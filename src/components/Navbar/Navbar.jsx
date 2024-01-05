0e31d343b8f5fa1f6af623091bc5bcb44c14c392
Lucca Nunes <lyulucca@gmail.com>
2024-01-05 20:07:49 -0300

    feat: title and image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import '../Navbar/navbar.css'
 const Navbar = () => {
   return (
     <nav id="nav-container">
-        <h1>Lucca Nunes</h1>
+        <Link><h1>Lucca Nunes</h1></Link>
         <ul>
-            <li><Link>Projetos</Link></li>
-            <li><Link>Sobre</Link></li>
-            <li><Link>Contato</Link></li>
+            <li><Link className='marked-link'>Projetos</Link></li>
+            <li><Link className='marked-link'>Sobre</Link></li>
+            <li><Link className='marked-link'>Contato</Link></li>
         </ul>
     </nav>
   )
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,7 +5,8 @@ const Inicio = () => {
   return (
     <main>
         <section className='title-container'>
-
+            <h1>Desenvolvedor <br />Front-end.</h1>
+            <img src="src/assets/me.png" alt="" />
         </section>
         <div className='work-together'>
             <h3>Vamos trabalhar <Link>juntos</Link></h3>
